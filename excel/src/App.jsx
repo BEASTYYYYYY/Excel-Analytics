b36b972d6bd2bb6ad706d8ba07c0f4f3c4400d01
Yash kasera <yashkasera07@gmail.com>
2025-06-05 18:57:46 +0530

    fix the api bug

--- excel/src/App.jsx
+++ excel/src/App.jsx
@@ -14,15 +14,16 @@ import Sidebar from "./components/Sidebar";
 import DashboardConfigurator from "./components/Configurator";
 import { colorOptions, typeOptions } from "./components/Configurator";
 import ConfigTool from "./components/ConfigTool";
+import process from "process";
 import AppRoutes from "./routes/AppRoutes";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDGd9lEUBaCB4yNGCF7IyNAlg23DgAZt6c",
-  authDomain: "excelwiz-dad69.firebaseapp.com",
-  projectId: "excelwiz-dad69",
-  storageBucket: "excelwiz-dad69.firebasestorage.app",
-  messagingSenderId: "1058594841348",
-  appId: "1:1058594841348:web:12b7663264c568bc3e6651"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 initializeApp(firebaseConfig);
