1a024fb38d85c79e7ace7251dd25a9e4f721814f
Yash kasera <yashkasera07@gmail.com>
2025-06-06 19:27:00 +0530

    updated server.js

--- excel/backend/server.js
+++ excel/backend/server.js
@@ -2,7 +2,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import password from './routes/password.js';
